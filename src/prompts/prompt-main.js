01bcbd9365507d35680e3354ed4ef2145d597037
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-21 14:34:22 -0300

    feat: creating the configuration file for the main prompt

---
+++ src/prompts/prompt-main.js
@@ -0,0 +1,9 @@
+const mainPrompt = [
+    {
+        name: "select",
+        description: "Escolha a ferramenta (1 - QRCODDE ou 2 - PASSWORD)",
+        pattern: /^[1-2]+$/,
+        message: "Escolha apenas 1 ou 2",
+        required: true
+    }
+]
