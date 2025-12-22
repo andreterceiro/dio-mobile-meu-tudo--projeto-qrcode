19b160ea34d5698252f2646da54ff301cf289a00
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-22 11:56:23 -0300

    feat: renaming

--- src/services/qr-code/create.js
+++ src/services/qr-code/create.js
@@ -1,6 +1,6 @@
 import prompt from 'prompt';
 import promptQRCode from '../../prompts/prompt-qrcode.js';
-import handler from './handle.js';
+import handler from './handler.js';
 
 async function createQRCode() {
     prompt.get(promptQRCode, handler);
