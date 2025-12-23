7109de7ecb6d187f1761a7bd728a0956c7cea969
André de Paula Terceiro <andre@terceiro.com.br>
2025-12-23 10:40:00 -0300

    feat: importing and using the file that provides the allowed characters

--- src/services/password/handler.js
+++ src/services/password/handler.js
@@ -1,28 +1,4 @@
-async function getPermittedCharacters() {
-    let characters = [];
-
-    if (process.env.UPPERCASE_LETTERS === "true") {
-        characters.push(... "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    }
-
-    if (process.env.LOWERCASE_LETTERS === "true") {
-        characters.push(... "abcdefghijklmnopqrstuvwxyz");
-    }
-
-    if (process.env.NUMBERS === "true") {
-        characters.push(... "0123456789");
-    }
-
-    if (process.env.NUMBERS === "true") {
-        characters.push(... "0123456789");
-    }
-
-    if (process.env.SPECIAL_CHARACTERS === "true") {
-        characters.push(... "!@#$%^&*()-_");
-    }
-
-    return characters;
-}
+import { getPermittedCharacters } from "./permitted-characters.js";
 
 async function handler() {
     let characters = await getPermittedCharacters();
